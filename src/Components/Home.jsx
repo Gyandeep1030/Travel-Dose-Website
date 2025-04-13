fb6ea807ca06e2470728daef23eb1f2b6c528689
Gyandeep <Gyandeep1030@gmail.com>
2025-04-13 14:00:24 +0530

    img bug

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,12 +4,13 @@ import Destination from './Destination';
 import RecentTrips from './RecentTrips';
 import Footer from './Footer';
 import TravelPackages from './TravelPackages';
+import img12 from '../assets/12.jpg';
 function Home() {
     return ( 
         <div >
             <HeroSection 
                 className = "heroHome"
-                img = "src/assets/12.jpg"
+                img = {img12}
                 title = "Welcome to Travel Dose"
                 description = "Discover the world, experience the magic, and uncover the hidden gems."
                 descriptionText = "HomeText"
